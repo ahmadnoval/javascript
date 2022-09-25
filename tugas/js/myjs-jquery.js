726d9d4505d46456751caf4cdc536c360aca4506
ahmadnoval <49505609+ahmadnoval@users.noreply.github.com>
2022-09-25 14:16:41 +0700

    Add files via upload

---
+++ tugas/js/myjs-jquery.js
@@ -0,0 +1,264 @@
+$("document").ready(function () {
+    
+    // Hewan Unggas
+
+    // <------------------------------AYAM------------------------------>
+
+    $("#ayam").click(function () {
+        $("#ayam-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#ayam-text").css({
+            'top': "75%"
+        })
+        $("#ayam-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/unggas/ayam.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------BEBEK------------------------------>
+
+    $("#bebek").click(function () {
+        $("#bebek-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#bebek-text").css({
+            'top': "75%"
+        })
+        $("#bebek-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/unggas/bebek.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------ELANG------------------------------>
+
+    $("#elang").click(function () {
+        $("#elang-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#elang-text").css({
+            'top': "75%"
+        })
+        $("#elang-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/unggas/eagle.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------ANGSA----------------------------->
+
+    $("#angsa").click(function () {
+        $("#angsa-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#angsa-text").css({
+            'top': "75%"
+        })
+        $("#angsa-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/unggas/angsa.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------BURUNG DARA------------------------------>
+
+    $("#dara").click(function () {
+        $("#dara-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#dara-text").css({
+            'top': "75%"
+        })
+        $("#dara-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/unggas/pigeon.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+
+    // Hewan Buas
+
+    // <------------------------------CHEETAH------------------------------>
+
+    $("#cheetah").click(function () {
+        $("#cheetah-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#cheetah-text").css({
+            'top': "75%"
+        })
+        $("#cheetah-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/buas/cheetah.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------SINGA------------------------------>
+
+    $("#singa").click(function () {
+        $("#singa-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#singa-text").css({
+            'top': "75%"
+        })
+        $("#singa-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/buas/singa.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })   
+
+    // <------------------------------BERUANG GRIZZELY------------------------------>
+
+    $("#bear").click(function () {
+        $("#bear-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#bear-text").css({
+            'top': "75%"
+        })
+        $("#bear-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/buas/beruang.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------KOMODO------------------------------>
+
+    $("#komodo").click(function () {
+        $("#komodo-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#komodo-text").css({
+            'top': "75%"
+        })
+        $("#komodo-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/buas/komodo.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------FOX------------------------------>
+
+    $("#fox").click(function () {
+        $("#fox-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#fox-text").css({
+            'top': "75%"
+        })
+        $("#fox-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/buas/fox.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // Hewan Ternak
+
+    // <------------------------------BANTENG------------------------------>
+
+    $("#buffalo").click(function () {
+        $("#buffalo-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#buffalo-text").css({
+            'top': "75%"
+        })
+        $("#buffalo-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/ternak/kerbau.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------SEPI------------------------------>
+
+    $("#cow").click(function () {
+        $("#cow-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#cow-text").css({
+            'top': "75%"
+        })
+        $("#cow-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/ternak/sapi.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })   
+
+    // <------------------------------KAMBING------------------------------>
+
+    $("#goat").click(function () {
+        $("#goat-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#goat-text").css({
+            'top': "75%"
+        })
+        $("#goat-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/ternak/kambing.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------KELINCI------------------------------>
+
+    $("#rabbit").click(function () {
+        $("#rabbit-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#rabbit-text").css({
+            'top': "75%"
+        })
+        $("#rabbit-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/ternak/kelinci.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    // <------------------------------BABI------------------------------>
+
+    $("#pig").click(function () {
+        $("#pig-img").attr({
+            'src': "https://dummyimage.com/600x400/23543/ff345f"
+        });
+        $("#pig-text").css({
+            'top': "75%"
+        })
+        $("#pig-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/ternak/BABI.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+});
